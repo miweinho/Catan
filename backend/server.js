1eb24e46e67c2ed4f594909ffb222618c51ab8a1
miweinho <weimic01@students.zhaw.ch>
2024-09-07 23:15:23 +0200

    adding atttack and reveal feature

--- backend/server.js
+++ backend/server.js
@@ -1,156 +1,326 @@
-const express = require('express');
-const path = require('path');
-const session = require('express-session');
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false } // Set to true if using https
-}));
+app.use(express.static("public"));
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, // Set to true if using https
+  })
+);
 
 // Define locations with ownership information
 let locations = [
-    { name: "Kirche Eichberg", lat: 47.346416, lon: 9.525444, currentOwner: null, visible: false },
-    { name: "Fussballfeld Lagerhaus", lat: 47.342758, lon: 9.513821, currentOwner: null, visible: false },
-    { name: "Sportplatz Dorf", lat: 47.345082, lon: 9.523095, currentOwner: null, visible: false },
-    { name: "Reservoir", lat: 47.342513, lon: 9.512209, currentOwner: null, visible: false },
-    { name: "Landgasthof Hölzlisberg", lat: 47.345040, lon: 9.513739, currentOwner: null, visible: false },
-    { name: "Zoologische Station", lat: 47.347345, lon: 9.525002, currentOwner: null, visible: false },
-    { name: "Kartonfabrik", lat: 47.339972, lon: 9.521770, currentOwner: null, visible: false },
-    { name: "Heiterhof", lat: 47.344916, lon: 9.525751, currentOwner: null, visible: false },
-    { name: "Seeli Ost", lat: 47.345663, lon: 9.535846, currentOwner: null, visible: false },
+  {
+    name: "Kirche Eichberg",
+    lat: 47.346416,
+    lon: 9.525444,
+    currentOwner: null,
+    visible: false,
+    lastAttack: null,
+  },
+  {
+    name: "Fussballfeld Lagerhaus",
+    lat: 47.342758,
+    lon: 9.513821,
+    currentOwner: null,
+    visible: false,
+    lastAttack: null,
+  },
+  {
+    name: "Sportplatz Dorf",
+    lat: 47.345082,
+    lon: 9.523095,
+    currentOwner: null,
+    visible: false,
+    lastAttack: null,
+  },
+  {
+    name: "Reservoir",
+    lat: 47.342513,
+    lon: 9.512209,
+    currentOwner: null,
+    visible: false,
+    lastAttack: null,
+  },
+  {
+    name: "Landgasthof Hölzlisberg",
+    lat: 47.34504,
+    lon: 9.513739,
+    currentOwner: null,
+    visible: false,
+    lastAttack: null,
+  },
+  {
+    name: "Zoologische Station",
+    lat: 47.347345,
+    lon: 9.525002,
+    currentOwner: null,
+    visible: false,
+    lastAttack: null,
+  },
+  {
+    name: "Kartonfabrik",
+    lat: 47.339972,
+    lon: 9.52177,
+    currentOwner: null,
+    visible: false,
+    lastAttack: null,
+  },
+  {
+    name: "Heiterhof",
+    lat: 47.344916,
+    lon: 9.525751,
+    currentOwner: null,
+    visible: false,
+    lastAttack: null,
+  },
+  {
+    name: "Seeli Ost",
+    lat: 47.345663,
+    lon: 9.535846,
+    currentOwner: null,
+    visible: false,
+    lastAttack: null,
+  },
 ];
 
 const maxDistance = 50; // Maximum distance in meters
 
+let attackPossible = false;
+
+const generalDefense = 0.95;
+
 // Define groups and their passwords (in a real app, use a database)
 const groups = {
-    "Red Team": { password: "red123", capturedLocations: [] },
-    "Blue Team": { password: "blue123", capturedLocations: [] },
-    "Green Team": { password: "green123", capturedLocations: [] },
-    "Orange Team": { password: "orange123", capturedLocations: [] },
-    "Purple Team": { password: "purple123", capturedLocations: [] },
-    "Yellow Team": { password: "yellow123", capturedLocations: [] }
+  "Red Team": { password: "red123", capturedLocations: [], defense: 0.95 },
+  "Blue Team": { password: "blue123", capturedLocations: [], defense: 0.95 },
+  "Green Team": { password: "green123", capturedLocations: [], defense: 0.95 },
+  "Orange Team": {
+    password: "orange123",
+    capturedLocations: [],
+    defense: 0.95,
+  },
+  "Purple Team": {
+    password: "purple123",
+    capturedLocations: [],
+    defense: 0.95,
+  },
+  "Yellow Team": {
+    password: "yellow123",
+    capturedLocations: [],
+    defense: 0.95,
+  },
+  Admin: { password: "admin123", capturedLocations: [], defense: 0.95 },
 };
 
-// Status page password (in a real app, use a more secure method)
-const statusPagePassword = "admin123";
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "frontend", "index.html"));
 });
 
-app.get('/groups', (req, res) => {
-    res.json(Object.keys(groups));
+app.get("/groups", (req, res) => {
+  res.json(Object.keys(groups));
 });
 
-app.get('/status', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'frontend', 'status.html'));
+app.get("/status", (req, res) => {
+  if (req.session.group === "Admin") {
+    res.sendFile(path.join(__dirname, "..", "frontend", "status.html"));
+  } else {
+    res.status(403).json({ success: false, message: "Access denied" });
+  }
 });
 
-app.post('/reset', (req, res) => {
-    locations = locations.map(location => ({ ...location, currentOwner: null }));
+app.post("/reset", (req, res) => {
+  if (req.session.group !== "Admin") {
+    return res.status(403).json({ success: false, message: "Access denied" });
+  } else {
+    locations = locations.map((location) => ({
+      ...location,
+      currentOwner: null,
+        visible: false,
+    }));
     for (const group in groups) {
-        groups[group].capturedLocations = 0;
+      groups[group].capturedLocations = 0;
     }
     res.json({ success: true, message: "Game reset successfully" });
+  }
 });
 
-app.post('/login', async (req, res) => {
-    const { group, password } = req.body;
-    if (groups[group] && password === groups[group].password) {
-        req.session.group = group;
-        res.json({ success: true, message: "Logged in successfully" });
+app.post("/reveal", (req, res) => {
+  if (req.session.group !== "Admin") {
+    return res.status(403).json({ success: false, message: "Access denied" });
+  } else {
+    locations = locations.map((location) => ({
+      ...location,
+      visible: true,
+    }));
+    res.json({ success: true, message: "Location reveal successfully" });
+  }
+});
+
+app.post("/attack", (req, res) => { 
+    if(req.session.group === "Admin") {
+        attackPossible = true;
+        res.json({ success: true, message: "Attack mode activated" });
+    }
+});
+
+app.post("/login", async (req, res) => {
+  const { group, password } = req.body;
+  if (groups[group] && password === groups[group].password) {
+    req.session.group = group;
+    if (group === "Admin") {
+      res.json({
+        success: true,
+        message: "Logged in successfully",
+        statusAccess: true,
+        link: "/status",
+      });
     } else {
-        res.status(401).json({ success: false, message: "Invalid credentials" });
+      res.json({ success: true, message: "Logged in successfully" });
     }
+  } else {
+    res.status(401).json({ success: false, message: "Invalid credentials" });
+  }
 });
 
-app.get('/logout', (req, res) => {
-    req.session.destroy();
-    res.json({ success: true, message: "Logged out successfully" });
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.json({ success: true, message: "Logged out successfully" });
 });
 
-app.post('/api/check-status-password', (req, res) => {
-    if (req.body.password === statusPagePassword) {
-        req.session.statusAccess = true;
-        res.json({ success: true });
-    } else {
-        res.status(401).json({ success: false, message: "Invalid password" });
-    }
+app.get("/api/location-status", (req, res) => {
+  if (req.session.group) {
+    res.json(mapVisibleLocations(req.session.group));
+  } else {
+    res.status(403).json({ success: false, message: "Access denied" });
+  }
 });
 
-app.get('/api/location-status', (req, res) => {
-    if (req.session.statusAccess) {
-        res.json(locations.map(({ name, lat, lon, currentOwner }) => ({ name, lat, lon, currentOwner })));
-    } else {
-        res.status(403).json({ success: false, message: "Access denied" });
-    }
+app.get("/api/group-status", (req, res) => {
+  if (req.session.statusAccess) {
+    res.json(
+      Object.keys(groups).map((group) => ({
+        group,
+        capturedLocations: groups[group].capturedLocations,
+      }))
+    );
+  } else {
+    res.status(403).json({ success: false, message: "Access denied" });
+  }
 });
 
-app.post('/api/check-location', (req, res) => {
-    if (!req.session.group) {
-        return res.status(401).json({ success: false, message: "Not logged in" });
+app.post("/api/check-location", (req, res) => {
+  if (!req.session.group) {
+    return res.status(401).json({ success: false, message: "Not logged in" });
+  }
+
+  const { lat, lon } = req.body;
+  const group = req.session.group;
+  console.log(`Received location: ${lat}, ${lon} from group: ${group}`);
+
+  let nearestLocation = null;
+  let shortestDistance = Infinity;
+
+  for (const location of locations) {
+    const distance = calculateDistance(lat, lon, location.lat, location.lon);
+    if (distance < shortestDistance) {
+      shortestDistance = distance;
+      nearestLocation = location;
     }
+  }
 
-    const { lat, lon } = req.body;
-    const group = req.session.group;
-    console.log(`Received location: ${lat}, ${lon} from group: ${group}`);
+  let message;
+  if (shortestDistance <= maxDistance) {
+    message = captureLocation(group, nearestLocation);
+  } else {
+    message = `Du bist nicht in der Nähe einer Position!`;
+  }
 
-    let nearestLocation = null;
-    let shortestDistance = Infinity;
-
-    for (const location of locations) {
-        const distance = calculateDistance(lat, lon, location.lat, location.lon);
-        if (distance < shortestDistance) {
-            shortestDistance = distance;
-            nearestLocation = location;
-        }
-    }
-
-    let message;
-    if (shortestDistance <= maxDistance) {
-        if (nearestLocation.currentOwner === group) {
-            message = `Your group (${group}) already owns ${nearestLocation.name}!`;
-        } else {
-            if (nearestLocation.currentOwner) {
-                groups[nearestLocation.currentOwner].capturedLocations--;
-            }
-            nearestLocation.currentOwner = group;
-            groups[group].capturedLocations++;
-            message = `Congratulations! Your group (${group}) has taken over ${nearestLocation.name}!`;
-        }
-    } else {
-        message = `You are not within range of any known location.`;
-    }
-
-    res.json({
-        message: message,
-        capturedLocations: groups[group].capturedLocations
-    });
+  res.json({
+    message: message,
+    location: nearestLocation,
+  });
 });
 
 function calculateDistance(lat1, lon1, lat2, lon2) {
-    const R = 6371e3; // Earth's radius in meters
-    const φ1 = lat1 * Math.PI / 180;
-    const φ2 = lat2 * Math.PI / 180;
-    const Δφ = (lat2 - lat1) * Math.PI / 180;
-    const Δλ = (lon2 - lon1) * Math.PI / 180;
+  const R = 6371e3; // Earth's radius in meters
+  const φ1 = (lat1 * Math.PI) / 180;
+  const φ2 = (lat2 * Math.PI) / 180;
+  const Δφ = ((lat2 - lat1) * Math.PI) / 180;
+  const Δλ = ((lon2 - lon1) * Math.PI) / 180;
 
-    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-              Math.cos(φ1) * Math.cos(φ2) *
-              Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const a =
+    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-    return R * c;
+  return R * c;
+}
+// Check if a group can capture a location and
+function captureLocation(group, location) {
+  if (location.currentOwner === null) {
+    if (isAttackSuccessfull(location)) {
+      location.currentOwner = group;
+      groups[group].capturedLocations.push(location);
+      location.visible = true;
+      return `Gratuliere deine Gruppe (${group}) hat die Position ${
+        location.name
+      } übernommen! Du hattest eine Chance von ${generalDefense * 100}%`;
+    } else {
+      return `Deine Gruppe (${group}) hat die Position ${
+        location.name
+      } nicht übernommen! Du hattest eine Chance von ${generalDefense * 100}%`;
+    }
+  } else {
+    if (attackPossible) {
+      if (isAttackSuccessfull(location)) {
+        let previousOwner = groups[location.currentOwner];
+        delete groups[location.currentOwner].capturedLocations[location.name];
+        location.currentOwner = group;
+        groups[group].capturedLocations.push(location);
+        return `Gratuliere deine Gruppe (${group}) hat die Position ${
+          location.name
+        } übernommen! Du hattest eine Chance von ${
+          previousOwner.defense * 100
+        }%`;
+      } else {
+        
+        return `Deine Gruppe (${group}) hat die Position ${
+          location.name
+        } nicht übernommen! Du hattest eine Chance von ${
+          previousOwner * 100
+        }%`;
+      }
+    } else {
+      return `Die Position ${location.name} gehört bereits ${location.currentOwner}. Du kannst momentan noch nicht angreifen!`;
+    }
+  }
+}
+
+function isAttackSuccessfull(location) {
+  if (location.currentOwner === null) {
+    return Math.random() < generalDefense;
+  } else {
+    return Math.random() < groups[location.currentOwner].defense;
+  }
+}
+
+function mapVisibleLocations(group) {
+  if (group === "Admin") {
+    return locations.filter((location) => location.visible);
+  } else {
+    return locations.filter((location) => location.currentOwner === group);
+  }
 }
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
